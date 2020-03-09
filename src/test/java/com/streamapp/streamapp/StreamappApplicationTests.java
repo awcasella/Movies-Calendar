bbb2739a9626a40129d86afe9ebdbf7d8cd29be1
awcasella <awcasella461@gmail.com>
2020-03-08 21:25:30 -0300

    Simple  commit

---
+++ src/test/java/com/streamapp/streamapp/StreamappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streamapp.streamapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
